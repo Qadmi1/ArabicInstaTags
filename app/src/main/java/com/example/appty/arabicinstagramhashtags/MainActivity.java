602754e8812aff4fbbe38774bef029e3df6a002f
pyoxiyo <pyoxiyo@gmail.com>
2018-06-28 16:25:58 +0300

    Applied URL Builder and now I can add more categories and now I made sure the Loader will start only if we have a connection.

--- app/src/main/java/com/example/appty/arabicinstagramhashtags/MainActivity.java
+++ app/src/main/java/com/example/appty/arabicinstagramhashtags/MainActivity.java
@@ -1,24 +1,29 @@
 package com.example.appty.arabicinstagramhashtags;
 
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Loader;
-import android.os.AsyncTask;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.example.appty.arabicinstagramhashtags.vo.HashTag;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<String>>{
-    static final String INSTA_URL ="https://api.instagram.com/v1/tags/search?q=حلا&access_token=" +
-            "4580320737.5854d71.7ad20f499f1f4ee692c078bf186335dd";
+    static final String INSTA_URL ="https://api.instagram.com/v1/tags/search";
+
+
     ArrayAdapter<String> adapter;
     private ListView listView;
     private static final int LOADER_ID = 1;
+    private static final String ACCESS_TOKEN = "4580320737.5854d71.7ad20f499f1f4ee692c078bf186335dd";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,18 +35,40 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, new ArrayList<String>());
         listView.setAdapter(adapter);
 
-        LoaderManager loaderManager = getLoaderManager();
-        // Initialize the loader to create a new loader or use the existing one.
-        loaderManager.initLoader(LOADER_ID, null, this);
+        /**
+         * *****************************************************************************************
+         * ******************************* MUST REVIEW *********************************************
+         * *****************************************************************************************
+         */
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        assert cm != null;
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+
+        // Make sure there is a connection before fetching the data
+        if (activeNetwork != null && activeNetwork.isConnected()) {
+            // Create a reference to the LoaderManager
+            LoaderManager loaderManager = getLoaderManager();
+            // Initialize the loader to create a new loader or use the existing one.
+            loaderManager.initLoader(LOADER_ID, null, this);
+        }
+        /**
+         * ************** Add else here to handle the empty view if you will use it ****************
+         */
     }
 
     @Override
     public Loader<List<String>> onCreateLoader(int id, Bundle args) {
 
 
+        Uri baseUri = Uri.parse(INSTA_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
 
+        uriBuilder.appendQueryParameter("q", "طب");
+        uriBuilder.appendQueryParameter("access_token", ACCESS_TOKEN);
 
-        return new TagsLoader(this, INSTA_URL);
+        return new TagsLoader(this, uriBuilder.toString());
     }
 
     @Override
@@ -56,7 +83,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public void onLoaderReset(Loader<List<String>> loader) {
-
+        adapter.clear();
     }
 
 
